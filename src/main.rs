99f76ca718da6fbfaffd95830b58562defc708bd
baltol <smith@example.com>
2021-04-17 16:09:57 -0400

    Create a cli that only outputs an emoji of the current moon phase

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// Prints today's lunar phase
+
+use chrono::{Datelike, Local, TimeZone};
+use esbat::daily_lunar_phase;
+
+fn main() {
+    let today = Local::today();
+    let day = Local.ymd(today.year(), today.month(), 1);
+    let phase = daily_lunar_phase(day);
+    print!("{}", phase.as_emoji());
+}
