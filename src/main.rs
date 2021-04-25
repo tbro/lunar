ab269cf17902413decb56efead5ffffbee3c9f55
baltol <smith@example.com>
2021-04-25 12:59:24 -0400

    Include day in calculation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use esbat::daily_lunar_phase;
 
 fn main() {
     let today = Local::today();
-    let day = Local.ymd(today.year(), today.month(), 1);
+    let day = Local.ymd(today.year(), today.month(), today.day());
     let phase = daily_lunar_phase(day);
     print!("{}", phase.as_emoji());
 }
